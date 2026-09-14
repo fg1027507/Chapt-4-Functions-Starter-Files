938897dc0a212f201ddc82260c3de12339dcf4b2
fg1027507 <fg1027507@otc.edu>
2026-09-14 09:28:58 -0500

    updated php functions work

--- function.php
+++ function.php
@@ -1,11 +1,20 @@
 <?php
     date_default_timezone_set("America/Chicago");
+    $timeStamp = time();
     function todaysDate() {
-        $timeStamp = time();
+        global $timeStamp;
         echo ("Todays date is: " . date($timeStamp) . ". ");
         echo "Todays date is: " . date("M-d-Y g:i:s A T", $timeStamp);
         echo (date_default_timezone_get());
     }
-    todaysDate();
+    // todaysDate();
+    function calculateFutureDate($daysToAdd) {
+        global $timeStamp;
+        $futureTimeStamp = strtotime("+$daysToAdd days", $timeStamp);
+        // echo ($futureTimeStamp);
+        $futureDate = date("Y-m-d D", $futureTimeStamp);
+        echo ("The date $daysToAdd days from now is: " . $futureDate);
+    }
+    calculateFutureDate(5);
 ?>
 
